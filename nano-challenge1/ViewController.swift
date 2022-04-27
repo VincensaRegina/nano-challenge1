da0395c3ec2500cf10d434f466b787b268cb59e3
Vincensa Regina Indarto <vincensaregina@gmail.com>
2022-04-28 00:09:43 +0700

    Initial Commit

---
+++ nano-challenge1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nano-challenge1
+//
+//  Created by Vincensa Regina on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
